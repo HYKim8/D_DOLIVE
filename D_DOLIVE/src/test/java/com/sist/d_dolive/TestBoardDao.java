192125c942781b26084c391601e38b2fd8b3a72b
sist <sist@DESKTOP-2UN23EM>
2020-05-08 12:47:51 +0900

    테스트 폴더 추가

---
+++ D_DOLIVE/src/test/java/com/sist/d_dolive/TestBoardDao.java
@@ -0,0 +1,46 @@
+package com.sist.d_dolive;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		                           "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+                                   })
+public class TestBoardDao {
+
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		LOG.debug("=========================");
+		LOG.debug("=test()=");
+		LOG.debug("=========================");
+	}
+	
+}
